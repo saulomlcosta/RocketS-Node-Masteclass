f74b5a9d22ec47093ae3e9cd7242f81c3028b0de
Saulo Costa <saulomlcosta10@gmail.com>
2021-09-05 10:36:32 -0300

    Http with fs and path

--- http/server.js
+++ http/server.js
@@ -1,9 +1,25 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 http.createServer((req, res) => {
-  if(req.url === '/')
-    return res.end('<h1>HomePage</h1>');
 
-  if(req.url === '/contato')
-    return res.end('<h1>Contacts</h1>');
+  const file = req.url ==='/' ? 'index.html' : req.url;
+  const filePath = path.join(__dirname, 'public', file);
+  const extname= path.extname(filePath);
+
+  const allowedFileTypes = ['.html', '.css', '.js'];
+  const allowed = allowedFileTypes.find(item => item == extname);
+
+  if(!allowed) return
+
+    fs.readFile(
+      filePath,
+      (err, content) => {
+        if(err) throw err
+
+        res.end(content)
+      }
+    )  
+   
 }).listen(5000, () => console.log('Server is running!'));
